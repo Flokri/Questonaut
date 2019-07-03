cbd602e6d079b2cddc6f4659a6d1c74fb8b616f6
Florian Kriegl <floriankriegl@gmx.at>
2019-07-03 16:27:39 +0200

    Add the AppCenter Services to the app.

--- Questonaut/App.xaml.cs
+++ Questonaut/App.xaml.cs
@@ -2,6 +2,11 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+//AppCenter usings
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 namespace Questonaut
 {
     public partial class App : Application
@@ -15,7 +20,11 @@ namespace Questonaut
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            //Start the appcenter services
+            AppCenter.Start("ios={enter iOS App seccret here};" +
+                  "uwp={enter UWP App seccret here};" +
+                  "android={enter Android App seccret here}",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
