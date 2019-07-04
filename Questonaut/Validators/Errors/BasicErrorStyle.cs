c2c1ac7b401794326843c629a4fad0b59b022aa5
Florian Kriegl <floriankriegl@gmx.at>
2019-07-04 23:13:53 +0200

    Added behaviors and validators.

---
+++ Questonaut/Validators/Errors/BasicErrorStyle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Questonaut.Validators.Errors
+{
+    public class BasicErrorStyle : IErrorStyle
+    {
+        #region Instances
+        #endregion
+
+        #region Constructor
+        public BasicErrorStyle()
+        {
+        }
+        #endregion
+
+        #region PublicMethods
+        public void RemoveError(View view)
+        {
+            StackLayout layout = view.Parent as StackLayout;
+            int viewIndex = layout.Children.IndexOf(view);
+
+            if (viewIndex + 1 < layout.Children.Count)
+            {
+                View sibling = layout.Children[viewIndex + 1];
+                string siblingStyleId = view.Id.ToString();
+
+                if (sibling.StyleId == siblingStyleId)
+                {
+                    sibling.IsVisible = false;
+                }
+            }
+        }
+
+        public void ShowError(View view, string message)
+        {
+            StackLayout layout = view.Parent as StackLayout;
+            int viewIndex = layout.Children.IndexOf(view);
+
+            if (viewIndex + 1 < layout.Children.Count)
+            {
+                View sibling = layout.Children[viewIndex + 1];
+                string siblingStyleId = view.Id.ToString();
+
+                //Reuse the existing label
+                if (sibling.StyleId == siblingStyleId)
+                {
+                    Label errorLabel = sibling as Label;
+                    errorLabel.Text = message;
+                    errorLabel.IsVisible = true;
+
+                    return;
+                }
+
+            }
+
+            //Add new label if none exists
+            layout.Children.Insert(viewIndex + 1, new Label
+            {
+                Text = message,
+                FontSize = 10,
+                StyleId = view.Id.ToString(),
+                TextColor = Color.Red,
+            });
+        }
+        #endregion
+
+        #region PrivateMethods
+        #endregion
+
+        #region Properties
+        #endregion
+    }
+}
---
+++ Questonaut/Validators/FormatValidator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Questonaut.Validators
+{
+    public class FormatValidator : IValidator
+    {
+        #region Instances
+        #endregion
+
+        #region Constructor
+        public FormatValidator()
+        {
+        }
+        #endregion
+
+        #region PublicMethods
+        public bool Check(string value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                Regex format = new Regex(Format);
+
+                return format.IsMatch(value);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Properties
+        public string Message { get; set; } = "Invalid format";
+        public string Format { get; set; }
+        #endregion
+    }
+}
