76416a6f8e6786f2ea0de6c142ab1cbed9f1ba24
Florian Kriegl <floriankriegl@gmx.at>
2019-07-07 17:28:33 +0200

    Fixed a text in the ios pre build script and modified the android prebuild script.

--- Questonaut.Android/appcenter-pre-build.sh
+++ Questonaut.Android/appcenter-pre-build.sh
@@ -1,3 +1,4 @@
-echo "Injecting secrets..."
-echo "Updating Google plist"
-echo $GOOGLE_SERVICES_PLIST | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/ios/GoogleService-Info.plist"
+echo "Injecting firestore secrets..."
+echo "Updating android secret"
+echo $ANDROID_SECRET > "$APPCENTER_SOURCE_DIRECTORY/Questonaut.Android/appcenter-config.json"
+echo "Finished injecting secrets."
--- Questonaut.iOS/appcenter-pre-build.sh
+++ Questonaut.iOS/appcenter-pre-build.sh
@@ -1,4 +1,4 @@
-echo "Injecting secrets..."
+echo "Injecting firestore secrets..."
 echo "Updating Google plist"
 echo $GOOGLE_SERVICES_PLIST | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/Questonaut.iOS/GoogleService-Info.plist"
 echo "Finished injecting secrets."
