628d9fed9d63b1f97f00f6eb7f64bdd6ada925d5
Florian Kriegl <floriankriegl@gmx.at>
2019-07-24 17:27:31 +0200

    Fixed the Bug that the controls can not be tapped (Android)

--- Questonaut/views/MainView.xaml.cs
+++ Questonaut/views/MainView.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Questonaut.Views
@@ -10,6 +10,14 @@ namespace Questonaut.Views
         public MainView()
         {
             InitializeComponent();
+        }
+
+        async void Handle_Tapped_Visual(object sender, System.EventArgs e)
+        {
+            var fr = ((Frame)sender).BackgroundColor;
+            ((Frame)sender).BackgroundColor = Color.FromHex("#F3F3F3");
+            await Task.Delay(100);
+            ((Frame)sender).BackgroundColor = fr;
         }
     }
 }
